2799288d8debca82ace55bb572f6bf820b4c116a
Tomas Dostal <Dostal.Tomas@gmail.com>
2019-03-05 17:00:39 +0100

    TS-REFACTORING: Refactoring of demo

--- demo/app.ts
+++ demo/app.ts
@@ -1,14 +1,11 @@
-import register from 'angularjs-register';
+import register from '@kpsys/angularjs-register';
 import {DateTime, Settings} from 'luxon';
-// @ts-ignore
-import datetimePickerModule from '../../../dist/datetimepicker';
 
-import 'bootstrap/less/bootstrap.less';
-import '../../../dist/datetimepicker.css';
+import datetimePickerModule from './../dist/angularjs-bootstrap-datetimepicker';
+import './../dist/angularjs-bootstrap-datetimepicker.css';
 
 import DemoController from './demo.controller';
 
-// @ts-ignore
 Settings.defaultLocale = DateTime.local().resolvedLocaleOpts().locale;
 
 register('demo', [datetimePickerModule])
